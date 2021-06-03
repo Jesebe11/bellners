723df832343b67c96d8eb6b4ac822534c78dfe55
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-03 17:39:12 -0400

    Created shortcode for newsletter form

--- template-parts/components/content-footer/newsletter.php
+++ template-parts/components/content-footer/newsletter.php
@@ -2,4 +2,9 @@
     <h1 class="newsletter__title">
         Sign up for our newsletter to stay up to date with tech news!
     </h1>
+    <div class="newsletter-form">
+        <?php
+            echo do_shortcode( '[contact-form-7 id="54" title="Newsletter"]' );
+        ?>
+    </div>
 </section>
