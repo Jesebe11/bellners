4c8b0951e8c95408062b1c19cf3fa50dd4046573
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-10 17:31:57 -0400

    Finished query for post type blogs and was added your respectives class css

--- template-parts/components/blocks-home/b-h-blog.php
+++ template-parts/components/blocks-home/b-h-blog.php
@@ -12,18 +12,31 @@ $args = [
 $the_query = new WP_Query( $args);
 
 ?>
+<section class="block-home__blog">
 
         <?php if( $the_query->have_posts() ) : ?>
             <?php while( $the_query->have_posts() ) :
                 $the_query->the_post();
-
                 $title = get_the_title();
                 $permalink = esc_url(get_the_permalink());
                 $excerpt = get_the_excerpt();
             ?>
+                <div class="item">
+                    <?= get_the_post_thumbnail( $post->ID, 'medium' ); ?>
+                    <div class="item__info">
+                        <p><?  echo get_the_date( 'ymd' ) ?></p>
+                        <p> <? the_tags(  ) ?>
+                    </div>
+                    <a class="item__title" href=" <?= $permalink; ?> ">
+                        <h2><?= $title; ?></h2>
+                    </a>
+                    <p class="item__excerpt">
+                        <? $excerpt; ?>
+                    </p>
+                </div>
 
-<?= $title; ?>
 
 <?php endwhile;
             wp_reset_postdata();
         endif; ?>
+</section>
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -3,7 +3,7 @@
     get_template_part( 'template-parts/components/content-home/slider-primary' );
 
 ?>
-<section class="block-home">
+<div class="block-home">
     <div class="block-home-title">Blogs</div>
     <?php get_template_part( 'template-parts/components/blocks-home/b-h-blog' ); ?>
-</section>
+</div>
