73d865631b73b8cbd60758769610e75046acfd56
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-10 13:53:36 -0400

    Finished block for license

--- template-parts/components/content-footer/copyright.php
+++ template-parts/components/content-footer/copyright.php
@@ -1,5 +1,5 @@
 <div class="copyright">
-    <p>© 2021 All rights reserved by
-        <a href="#">BellNers</a>
+    <p class="copyright__info">© 2021 All rights reserved by
+        <a class="link" href="#">BellNers</a>
     </p>
 </div>
