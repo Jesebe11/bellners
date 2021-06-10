50920ee03c543f3cf1c919ed84caf48498ab1d48
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-10 13:18:27 -0400

    Created directories for warning license

---
+++ template-parts/components/content-footer/copyright.php
@@ -0,0 +1,5 @@
+<div class="copyright">
+    <p>© 2021 All rights reserved by
+        <a href="#">BellNers</a>
+    </p>
+</div>
--- template-parts/content-footer.php
+++ template-parts/content-footer.php
@@ -1,7 +1,4 @@
 <?php
-
-get_template_part( 'template-parts/components/content-footer/newsletter' ); 
-?>
-
-<?
-get_template_part( 'template-parts/components/content-footer/cols-navs' );?>
+    get_template_part( 'template-parts/components/content-footer/newsletter' );
+    get_template_part( 'template-parts/components/content-footer/cols-navs' );
+    get_template_part( 'template-parts/components/content-footer/copyright' );
