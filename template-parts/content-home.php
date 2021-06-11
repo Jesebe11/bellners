34d6166b6e5b4704b13897e09d680e53257bc52b
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-11 16:56:06 -0400

    Finished styles for titles blocks

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -4,6 +4,11 @@
 
 ?>
 <div class="block-home">
-    <div class="block-home-title">Blogs</div>
+    <div class="block-home-title">
+        Blogs
+        <a href="#">
+            <img src="https://i.postimg.cc/7hBtnNXT/fast-forward-double-right-arrows-symbol.png" alt="Ir">
+        </a>
+    </div>
     <?php get_template_part( 'template-parts/components/content-home/blocks-home/b-h-blog' ); ?>
 </div>
