77bc1f0bd8111f8451302358a7eefc1325fa2ebd
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-21 16:17:04 -0400

    Fixed some styles into button for cards

--- template-parts/components/content-home/titles-home/titles-home.php
+++ template-parts/components/content-home/titles-home/titles-home.php
@@ -1,7 +1,5 @@
 <div class="block-home-title">
-        <?=
-            $args['title'];
-        ?>
+        <?= $args['title']; ?>
         <a href="#"><img src="https://i.postimg.cc/7hBtnNXT/fast-forward-double-right-arrows-symbol.png" alt="Ir"></a>
 
     </div>
