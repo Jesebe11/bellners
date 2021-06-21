ef9d51064725aa271a35c3d9e7d4c8f733c01fad
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-21 13:19:43 -0400

    Finished block for services

--- template-parts/components/content-home/blocks-home/b-h-service.php
+++ template-parts/components/content-home/blocks-home/b-h-service.php
@@ -1,3 +1,108 @@
-<div class="hola">
-    hola
-</div>
+<div class="container__card">
+
+            <div class="card__father">
+                <div class="card">
+                    <div class="card__front" style="background-image: url(https://i.postimg.cc/GmhxD7gc/business-3639451-640.jpg);">
+                        <div class="bg"></div>
+                        <div class="body__card_front">
+                            <h1>LOGOS Y BRANDING</h1>
+                        </div>
+                    </div>
+                    <div class="card__back">
+                        <div class="body__card_back">
+                            <h1>LOGOS Y BRANDING</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ab quas recusandae voluptatum aliquid tempore animi corporis voluptas. Tempore neque iure necessitatibus voluptas nesciunt animi dolores incidunt delectus sapiente illum.</p>
+                            <input type="button" value="Leer Más">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card__father">
+                <div class="card">
+                    <div class="card__front" style="background-image: url(https://i.postimg.cc/3NsdnRYj/webdesign-3411373-640.jpg);">
+                        <div class="bg"></div>
+                        <div class="body__card_front">
+                            <h1>SITIOS WEB</h1>
+                        </div>
+                    </div>
+                    <div class="card__back">
+                        <div class="body__card_back">
+                            <h1>SITIOS WEB</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ab quas recusandae voluptatum aliquid tempore animi corporis voluptas. Tempore neque iure necessitatibus voluptas nesciunt animi dolores incidunt delectus sapiente illum.</p>
+                            <input type="button" value="Leer Más">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card__father">
+                <div class="card">
+                    <div class="card__front" style="background-image: url(https://i.postimg.cc/8CZgT0Fx/home-office-336377-640.jpg);">
+                        <div class="bg"></div>
+                        <div class="body__card_front">
+                            <h1>DESARROLLO DE APPS</h1>
+                        </div>
+                    </div>
+                    <div class="card__back">
+                        <div class="body__card_back">
+                            <h1>DESARROLLO DE APPS</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ab quas recusandae voluptatum aliquid tempore animi corporis voluptas. Tempore neque iure necessitatibus voluptas nesciunt animi dolores incidunt delectus sapiente illum.</p>
+                            <input type="button" value="Leer Más">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card__father">
+                <div class="card">
+                    <div class="card__front" style="background-image: url(https://i.postimg.cc/FKrQ54Kj/digital-marketing-1725340-640.jpg);">
+                        <div class="bg"></div>
+                        <div class="body__card_front">
+                            <h1>OPTIMIZACIÓN SEO</h1>
+                        </div>
+                    </div>
+                    <div class="card__back">
+                        <div class="body__card_back">
+                            <h1>OPTIMIZACIÓN SEO</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ab quas recusandae voluptatum aliquid tempore animi corporis voluptas. Tempore neque iure necessitatibus voluptas nesciunt animi dolores incidunt delectus sapiente illum.</p>
+                            <input type="button" value="Leer Más">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card__father">
+                <div class="card">
+                    <div class="card__front" style="background-image: url(https://i.postimg.cc/vBXkdqBD/visa-3082813-640.jpg);">
+                        <div class="bg"></div>
+                        <div class="body__card_front">
+                            <h1>E-COMMERCE</h1>
+                        </div>
+                    </div>
+                    <div class="card__back">
+                        <div class="body__card_back">
+                            <h1>E-COMMERCE</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ab quas recusandae voluptatum aliquid tempore animi corporis voluptas. Tempore neque iure necessitatibus voluptas nesciunt animi dolores incidunt delectus sapiente illum.</p>
+                            <input type="button" value="Leer Más">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card__father">
+                <div class="card">
+                    <div class="card__front" style="background-image: url(https://i.postimg.cc/G3xmSG1f/Pay-Per-Click-Management-Toronto.jpg);">
+                        <div class="bg"></div>
+                        <div class="body__card_front">
+                            <h1>PAGOS POR CLICK</h1>
+                        </div>
+                    </div>
+                    <div class="card__back">
+                        <div class="body__card_back">
+                            <h1>PAGOS POR CLICK</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ab quas recusandae voluptatum aliquid tempore animi corporis voluptas. Tempore neque iure necessitatibus voluptas nesciunt animi dolores incidunt delectus sapiente illum.</p>
+                            <input type="button" value="Leer Más">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
