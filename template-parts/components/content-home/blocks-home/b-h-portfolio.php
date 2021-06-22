8b75daec3a0f3e1eb015256309c03e7af5c1e2b3
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-22 12:56:50 -0400

    Was finished block and styles for portfolio

--- template-parts/components/content-home/blocks-home/b-h-portfolio.php
+++ template-parts/components/content-home/blocks-home/b-h-portfolio.php
@@ -17,21 +17,20 @@ $the_query = new WP_Query( $args);
             <?php while( $the_query->have_posts() ) :
                 $the_query->the_post();
                 $title = get_the_title();
-                $permalink = esc_url(get_the_permalink());
             ?>
                 <div class="content">
                     <div class="content-overlay"></div>
                     <?= get_the_post_thumbnail( $post->ID, 'medium', array('class' => 'content-image') ); ?>
                         <h2 class="content-title"><?= $title; ?></h2>
                         <p class="tags"> <? the_tags( '', ', ', '<br />' ) ?> </p>
-                    <div class="content-details fadeIn-bottom">
-
-                        <p class="content-text">
-                            <a href=" <?= $permalink; ?> ">
-                                <? the_excerpt(  ); ?>
-                            </a>
-                        </p>
-                    </div>
+                        <div class="content-details fadeIn-bottom">
+                            <div class="content-text">
+                            <?  echo '<p class="excerpt">' . get_the_excerpt() . '</p>'; ?>
+                            <a href="<?php the_permalink(); ?>">
+                                    <img src="https://i.postimg.cc/5NCrq5nk/eye-1.png" alt="Eye">
+                                </a>
+                            </div>
+                        </div>
                 </div>
 
 
