0c434a348c6f8b4de87b6ac9a5c2a076b390599a
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-22 17:47:49 -0400

    Created structure for parallax

--- template-parts/components/content-home/blocks-home/b-h-parallax.php
+++ template-parts/components/content-home/blocks-home/b-h-parallax.php
@@ -1,17 +1,19 @@
 <div class="container-parallax">
 	<div class="parallax">
         <h1 class="parallax__title">Nuestros numeros</h1>
-            <div class="item">
-                <h4 class="item__number item__number--people">390</h4>
-                Clientes satisfechos
-            </div>
-            <div class="item">
-                <h4 class="item__number item__number--task">210</h4>
-                Tareas completadas
-            </div>
-            <div class="item">
-                <h4 class="item__number item__number--files">120</h4>
-                Proyectos completados
-            </div>
+            <section class="list">
+                <div class="list__item">
+                    <h4 class="list__item__number list__item__number--people">390</h4>
+                    Clientes satisfechos
+                </div>
+                <div class="list__item">
+                    <h4 class="list__item__number list__item__number--task">210</h4>
+                    Tareas completadas
+                </div>
+                <div class="list__item">
+                    <h4 class="list__item__number list__item__number--files">120</h4>
+                    Proyectos completados
+                </div>
+            </section>
     </div>
 </div>
