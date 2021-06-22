dade8d9fa81dd326b67017d5ce403a1345921ca0
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-22 16:40:43 -0400

    Reordered directories

---
+++ template-parts/components/content-home/blocks-home/b-h-parallax.php
@@ -0,0 +1,5 @@
+<div class="container-parallax">
+	<div class="parallax">
+        hola
+    </div>
+</div>
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -10,6 +10,19 @@
             get_template_part( 'template-parts/components/content-home/blocks-home/b-h-service' );
         get_template_part( 'template-parts/components/content-home/titles-home/titles-home' , '' , ['title' => 'Nuestro Portafolio'] );
             get_template_part( 'template-parts/components/content-home/blocks-home/b-h-portfolio' );
+
+    ?>
+</div>
+
+<?php
+
+    get_template_part( 'template-parts/components/content-home/blocks-home/b-h-parallax' );
+
+?>
+
+<div class="block-home">
+    <?php
+
         get_template_part( 'template-parts/components/content-home/titles-home/titles-home' , '' , ['title' => 'Nuestros clientes'] );
             get_template_part( 'template-parts/components/content-home/blocks-home/b-h-clients' );
         get_template_part( 'template-parts/components/content-home/titles-home/titles-home' , '' , ['title' => 'Blog'] );
