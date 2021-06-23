276877be286d9accfdaf0b82761b643c158d351d
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-23 16:36:35 -0400

    Finished parallax

--- template-parts/components/content-home/blocks-home/b-h-parallax.php
+++ template-parts/components/content-home/blocks-home/b-h-parallax.php
@@ -1,19 +1,18 @@
 <div class="container-parallax">
 	<div class="parallax">
-        <h1 class="parallax__title">Nuestros numeros</h1>
-            <section class="list">
-                <div class="list__item">
-                    <h4 class="list__item__number list__item__number--people">390</h4>
-                    Clientes satisfechos
-                </div>
-                <div class="list__item">
-                    <h4 class="list__item__number list__item__number--task">210</h4>
-                    Tareas completadas
-                </div>
-                <div class="list__item">
-                    <h4 class="list__item__number list__item__number--files">120</h4>
-                    Proyectos completados
-                </div>
-            </section>
+        <div class="parallax__info">
+            <h1 class="parallax__info__title">Nuestros numeros</h1>
+                <section class="list">
+                    <div class="list__item list__item--people">
+                        <h4 class="list__item__number"><span>390</span>Clientes satisfechos</h4>
+                    </div>
+                    <div class="list__item list__item--tasks">
+                        <h4 class="list__item__number"><span>210</span>Tareas completadas</h4>
+                    </div>
+                    <div class="list__item list__item--files">
+                        <h4 class="list__item__number"><span>120</span>Proyectos completados</h4>
+                    </div>
+                </section>
+        </div>
     </div>
 </div>
