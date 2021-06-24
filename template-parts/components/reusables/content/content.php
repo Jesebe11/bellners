40aae778f80743143f8fd79f05deafffac8253f6
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-24 10:15:15 -0400

    Finished terms and conditions page

---
+++ template-parts/components/reusables/content/content.php
@@ -0,0 +1,3 @@
+<div class="the-content">
+    <?php the_content( ); ?>
+</div>
--- template-parts/content-terms.php
+++ template-parts/content-terms.php
@@ -1,6 +1,6 @@
 <div class="block-general">
     <?php
         get_template_part( 'template-parts/components/reusables/titles/titles');
+        get_template_part( 'template-parts/components/reusables/content/content');
     ?>
-
 </div>
