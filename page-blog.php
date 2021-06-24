b77f497e57cf0d2631055179c6567e97e3f6ad8a
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-24 11:10:21 -0400

    Was created directories for page blogs  and was added a element css called split sections for sidebar

---
+++ page-blog.php
@@ -0,0 +1,15 @@
+<?php
+
+    get_header();
+?>
+
+	<main id="primary" class="main">
+
+		<?php
+            get_template_part( 'template-parts/content-blog' );
+		?>
+
+	</main>
+
+<?php
+get_footer();
