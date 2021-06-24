a38a0ee5529d2116d62dc7e33079d71a7e001663
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-24 16:18:26 -0400

    Was added area widgets for page blog

--- inc/widgets.php
+++ inc/widgets.php
@@ -28,6 +28,17 @@ function bellners_widgets_init() {
 			'after_title'   => '</h2>',
 		)
 	);
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Page Blogs', 'bellners' ),
+			'id'            => 'page-blogs',
+			'description'   => esc_html__( 'Add elements into page blogs', 'bellners' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
 }
 add_action( 'widgets_init', 'bellners_widgets_init' );
 ?>
--- template-parts/components/content-blog/sidebar.php
+++ template-parts/components/content-blog/sidebar.php
@@ -1,1 +1,5 @@
-sidebar
+<div>
+<?php if( is_active_sidebar( 'page-blogs' ) ) {
+            dynamic_sidebar( 'page-blogs' );
+        } ?>
+</div>
