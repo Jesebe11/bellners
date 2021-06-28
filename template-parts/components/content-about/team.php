8246a250c8e7b8fae6a336f6ec8362cce490675d
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-28 16:48:01 -0400

    Finished about us page

--- template-parts/components/content-about/team.php
+++ template-parts/components/content-about/team.php
@@ -1,1 +1,58 @@
-hola
+<section class="team">
+        <div class="team__list">
+
+            <div class="team__list__item">
+                <img src="https://i.postimg.cc/MG5zdCWc/192332820-3991190757632179-4550133225633761395-n.jpg" alt="" class="profile">
+                <h1 class="name">Nelson Zambrano</h1>
+                <h5 class="area">Desarrollador Web</h5>
+                <ul class="social">
+                    <li>
+                        <a href="">
+                            <img src="https://i.postimg.cc/4dQR1dck/linkedin.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="https://i.postimg.cc/RFBzffNL/github-logo-silhouette-in-a-square.png" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="team__list__item">
+                <img src="https://i.postimg.cc/2jWkXQpm/167793947-2283428361788428-1543586575805095413-n-1.jpg" alt="" class="profile">
+                <h1 class="name">Jesebell Escobar</h1>
+                <h5 class="area">Desarrolladora Web</h5>
+                <ul class="social">
+                    <li>
+                        <a href="">
+                            <img src="https://i.postimg.cc/4dQR1dck/linkedin.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="https://i.postimg.cc/RFBzffNL/github-logo-silhouette-in-a-square.png" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="team__list__item">
+                <img src="https://i.postimg.cc/0NJKHGWR/94758463-3232174343482726-8325026545395564544-n.jpg" alt="" class="profile">
+                <h1 class="name">Taner Alberto</h1>
+                <h5 class="area">Desarrollador Web</h5>
+                <ul class="social">
+                    <li>
+                        <a href="">
+                            <img src="https://i.postimg.cc/4dQR1dck/linkedin.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="https://i.postimg.cc/RFBzffNL/github-logo-silhouette-in-a-square.png" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+</section>
