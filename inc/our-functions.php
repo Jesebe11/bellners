d11afa83fd892fda548b3e082b2fdf50c6804c93
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-30 15:49:05 -0400

    Added a script for add a trigger over full image

--- inc/our-functions.php
+++ inc/our-functions.php
@@ -10,3 +10,23 @@ add_filter( 'get_the_archive_title', function ($title) {
     }
     return $title;
 });
+
+/** Woocommerce gallery trigger over full image */
+
+if( ! function_exists( 'WC_SCRIPT_in_wp_footer' ) && function_exists( 'is_product' ) ) {
+    /**
+     * Add script in the WP footer page.
+     */
+    function WC_SCRIPT_in_wp_footer() {
+        if ( is_product() && ! wp_is_mobile() ) {
+            ?>
+                <script type="application/javascript">
+                    jQuery( 'div.woocommerce-product-gallery.images' ).on( 'click', 'div.flex-viewport figure' , function(){
+                        jQuery( this ).closest( '.woocommerce-product-gallery' ).find( '.woocommerce-product-gallery__trigger' ).trigger( 'click' );
+                    });
+                </script>
+            <?php
+        }
+    }
+}
+add_action( 'wp_footer', 'WC_SCRIPT_in_wp_footer' );
