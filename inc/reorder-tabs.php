b480a2340c06baf54f212656b9d2a5dda3626c2f
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-01 10:16:42 -0400

    Reordered directories css

--- inc/reorder-tabs.php
+++ inc/reorder-tabs.php
@@ -8,7 +8,7 @@ function woo_reorder_tabs( $tabs ) {
 
     $tabs['description']['priority'] = 5;			// Description second
 	$tabs['reviews']['priority'] = 10;			// Reviews first
-	$tabs['additional_information']['priority'] = 15;	// Additional information third
+	//$tabs['additional_information']['priority'] = 15;	// Additional information third
 
 	return $tabs;
 }
