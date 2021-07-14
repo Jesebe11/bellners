837ff27c1aec170621a5d25d157a8bd1364cb265
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-14 15:37:31 -0400

    Was fixed some styles responsive into forms and was created directorie for overwrite dashboard.php

--- page-mi-cuenta.php
+++ page-mi-cuenta.php
@@ -9,9 +9,7 @@
             get_template_part( 'template-parts/components/reusables/titles/titles');
             ?>
             <img class="m-auto" style=" width:128px; height:128px;" src="https://i.postimg.cc/cJJG3kPR/profile-user.png" />
-
             <?php
-                //get_template_part( 'template-parts/content-contact' );
                 get_template_part( 'template-parts/components/reusables/content/content');
             ?>
     </div>
