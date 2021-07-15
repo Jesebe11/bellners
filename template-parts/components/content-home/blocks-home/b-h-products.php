089b9034608ae26654844522e734267b0dee320b
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-15 12:30:01 -0400

    Was been enhanced module for block products into home

--- template-parts/components/content-home/blocks-home/b-h-products.php
+++ template-parts/components/content-home/blocks-home/b-h-products.php
@@ -16,24 +16,23 @@ $the_query = new WP_Query( $args);
             <?php while( $the_query->have_posts() ) :
                 $the_query->the_post();
                 $title = get_the_title();
-                //$terms = get_the_terms( get_the_ID(), 'product_tag' );
             ?>
                 <div class="content">
                     <div class="content-overlay"></div>
                     <?= get_the_post_thumbnail( $post->ID, 'large', array('class' => 'content-image') ); ?>
-                        <h2 class="content-title"><?= $title; ?></h2>
-                        <?php
-                            $current_tags = get_the_terms( get_the_ID(), 'product_tag' );
-                                if ( $current_tags && ! is_wp_error( $current_tags ) ) {
-                                    echo '<ul class="product_tags">';
-                                        foreach ($current_tags as $tag) {
-                                            $tag_title = $tag->name;
-                                            $tag_link = get_term_link( $tag );
-                                            echo '<li><a href="'.$tag_link.'">'.$tag_title.'</a></li>';
+                        <div class="content__info">
+                            <h2 class="content-title"><?= $title; ?></h2>
+                            <?php
+                                $current_tags = get_the_terms( get_the_ID(), 'product_tag' );
+                                    if ( $current_tags && ! is_wp_error( $current_tags ) ) {
+                                            foreach ($current_tags as $tag) {
+                                                $tag_title = $tag->name;
+                                                $tag_link = get_term_link( $tag );
+                                                echo '<span class="tags"><a href="'.$tag_link.'">'.$tag_title.'</a></span>';
+                                            }
                                         }
-                                        echo '</ul>';
-                                    }
-                        ?>
+                            ?>
+                        </div>
                         <div class="content-details fadeIn-bottom">
                             <div class="content-text">
                             <?  echo '<p class="excerpt">' . get_the_excerpt() . '</p>'; ?>
