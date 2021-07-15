e5c671bd4b4bf312f71aaa6ce747bd9c83ff1611
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-15 12:32:48 -0400

    Change language copy section

--- template-parts/components/content-footer/copyright.php
+++ template-parts/components/content-footer/copyright.php
@@ -1,5 +1,5 @@
 <div class="copyright">
-    <p class="copyright__info">© 2021 All rights reserved by
+    <p class="copyright__info">© 2021 Todos los derechos reservados por
         <a class="link" href="#">BellNers</a>
     </p>
 </div>
