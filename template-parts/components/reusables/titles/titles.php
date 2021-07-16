00c00c5c41426f2d3744c51fc067fb04bdaa559c
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-16 16:41:03 -0400

    Was added links, and passed with parametres

--- template-parts/components/reusables/titles/titles.php
+++ template-parts/components/reusables/titles/titles.php
@@ -1,7 +1,7 @@
 <div class="block-general-title block-general-title--single">
         <?php if( is_home() ) :  ?>
         <?= $args['title']; ?>
-                <a href="#"><img src="https://i.postimg.cc/7hBtnNXT/fast-forward-double-right-arrows-symbol.png" alt="Ir"></a>
+                <a href="<?= $args['permalink']; ?> "><img src="https://i.postimg.cc/7hBtnNXT/fast-forward-double-right-arrows-symbol.png" alt="Ir"></a>
                 <?php elseif(is_archive ()) : ?>
                         <?php
                                 the_archive_title( '<h1>', '</h1>' );
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -6,9 +6,9 @@
 <div class="block-general">
     <?php
 
-        get_template_part( 'template-parts/components/reusables/titles/titles' , '' , ['title' => 'Servicios'] );
+        get_template_part( 'template-parts/components/reusables/titles/titles' , '' , ['title' => 'Servicios' , 'permalink' => '/index.php/category/servicios/'] );
             get_template_part( 'template-parts/components/content-home/blocks-home/b-h-service' );
-        get_template_part( 'template-parts/components/reusables/titles/titles' , '' , ['title' => 'Nuestros productos'] );
+        get_template_part( 'template-parts/components/reusables/titles/titles' , '' , ['title' => 'Nuestros productos' , 'permalink' => '/index.php/product-category/templates/'] );
             get_template_part( 'template-parts/components/content-home/blocks-home/b-h-products' );
 
     ?>
@@ -23,9 +23,9 @@
 <div class="block-general">
     <?php
 
-        get_template_part( 'template-parts/components/reusables/titles/titles' , '' , ['title' => 'Nuestros clientes'] );
+        get_template_part( 'template-parts/components/reusables/titles/titles' , '' , ['title' => 'Nuestros clientes' , 'permalink' => '/index.php/category/clientes/'] );
             get_template_part( 'template-parts/components/content-home/blocks-home/b-h-clients' );
-        get_template_part( 'template-parts/components/reusables/titles/titles' , '' , ['title' => 'Blog'] );
+        get_template_part( 'template-parts/components/reusables/titles/titles' , '' , ['title' => 'Blog' , 'permalink' => '/index.php/category/blogs/']  );
             get_template_part( 'template-parts/components/content-home/blocks-home/b-h-blog' );
 
     ?>
