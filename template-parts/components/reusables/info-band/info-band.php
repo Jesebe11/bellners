22719bafd1f56e0958b7ae03b1bc1b5789f4a352
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-16 13:59:14 -0400

    Finished info-brand styles

--- template-parts/components/reusables/info-band/info-band.php
+++ template-parts/components/reusables/info-band/info-band.php
@@ -3,21 +3,21 @@
         <img src="https://i.postimg.cc/zfb89Jp0/upload.png" alt="">
     </a>
     <ul class="info-band__list">
-        <li class="item">
-            <h1>Soporte 24/7</h1>
+        <li class="item spin circle">
             <img src="https://i.postimg.cc/15Cnp3dz/technical-support.png" alt="">
+            <h1>Soporte 24/7</h1>
         </li>
-        <li class="item">
-            <h1>Descuentos especiales</h1>
+        <li class="item spin circle">
             <img src="https://i.postimg.cc/BbN87SQM/gift-box.png" alt="">
+            <h1>Descuentos especiales</h1>
         </li>
-        <li class="item">
-            <h1>Pago Seguro</h1>
+        <li class="item spin circle">
             <img src="https://i.postimg.cc/9XBqQptZ/payment-security.png" alt="">
+            <h1>Pago Seguro</h1>
         </li>
-        <li class="item">
-            <h1>Diseños Personalizados</h1>
+        <li class="item spin circle">
             <img src="https://i.postimg.cc/ncc9jD05/idea.png" alt="">
+            <h1>Diseños Personalizados</h1>
         </li>
     </ul>
 </section>
