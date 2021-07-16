cec753ac34740a6e1eb831389d3995a176a56e33
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-16 11:38:49 -0400

    Created directories css for info band, and was changed button up of father

--- template-parts/components/reusables/info-band/info-band.php
+++ template-parts/components/reusables/info-band/info-band.php
@@ -1,4 +1,23 @@
-hola
-<a class="top-btn" href="#top">
+<section class="info-band">
+    <a class="top-btn" href="#top">
         <img src="https://i.postimg.cc/sf99dLG9/up-arrow.png" alt="">
     </a>
+    <ul class="info-band__list">
+        <li class="item">
+            <h1>Soporte 24/7</h1>
+            <img src="" alt="">
+        </li>
+        <li class="item">
+            <h1>Descuentos especiales</h1>
+            <img src="" alt="">
+        </li>
+        <li class="item">
+            <h1>Pago Seguro</h1>
+            <img src="" alt="">
+        </li>
+        <li class="item">
+            <h1>Diseños Personalizados</h1>
+            <img src="" alt="">
+        </li>
+    </ul>
+</section>
